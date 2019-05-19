bcacaf21449f11501fbf7b6c22a8981b526706b5
justinaus <justinaus2k@gmail.com>
2019-05-19 16:34:14 +0900

    Update connected count component

--- client/src/components/room_list/RoomListItem.tsx
+++ client/src/components/room_list/RoomListItem.tsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import styles from './RoomListItem.module.css'
 import { IRoom } from "../../interfaces/IRoom";
 import classNames from "classnames";
+import ConnectedCount from "../common/ConnectedCount";
 
 interface IProps {
   roomData: IRoom,
@@ -35,6 +36,14 @@ class RoomListItem extends Component<IProps, any> {
     } else {
       cn = classNames( styles.wrapper, styles.borderBottom );
     }
+
+    // 동적으로 가져와야 하는데 임시로 처리.
+    const gray = '#868e96';
+
+    const connectedCountStyle = {
+      'fontSize': '12px',
+      'color': gray
+    }
     
     return (
       <li className={ cn } onClick={ () => onClickItem( roomData ) }>
@@ -43,13 +52,10 @@ class RoomListItem extends Component<IProps, any> {
         </div>
         <div>
           <div className={ styles.title }>{ roomData ? roomData.title : '' }</div>
-          <div className={styles.users}>
-            <svg xmlns="http://www.w3.org/2000/svg" height="8" viewBox="0 0 12 6.5">
-              <path d="M4,7.5H2.5V6h-1V7.5H0v1H1.5V10h1V8.5H4ZM9,8a1.5,1.5,0,1,0-.455-2.93A2.467,2.467,0,0,1,9,6.5a2.516,2.516,0,0,1-.45,1.43A1.5,1.5,0,0,0,9,8ZM6.5,8A1.5,1.5,0,1,0,5,6.5,1.494,1.494,0,0,0,6.5,8ZM9.81,9.08a1.85,1.85,0,0,1,.69,1.42v1H12v-1C12,9.73,10.815,9.255,9.81,9.08ZM6.5,9c-1,0-3,.5-3,1.5v1h6v-1C9.5,9.5,7.5,9,6.5,9Z" transform="translate(0 -5)" fill='var(--gray)'/>
-            </svg>
-            <span>  </span>
-            { roomData && roomData.connected_count ? roomData.connected_count : 0 }명
-          </div>
+          <ConnectedCount 
+            count={ roomData && roomData.connected_count ? roomData.connected_count : 0 } 
+            iconColor={ gray }
+            objStyle={ connectedCountStyle } />
         </div>
       </li>
     );
