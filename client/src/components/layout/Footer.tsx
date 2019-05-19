2811ac4d6a98d10ae6f8381b827916aca33443e1
justinaus <justinaus2k@gmail.com>
2019-05-19 16:31:38 +0900

    Refactor remove console.log

--- client/src/components/layout/Footer.tsx
+++ client/src/components/layout/Footer.tsx
@@ -2,12 +2,6 @@ import React, { Component } from "react";
 import styles from './Footer.module.css'
 
 class Footer extends Component {
-  constructor( props: any  ) {
-    super( props );
-
-    console.log( 'footer' );
-  }
-
   render() {
     return (
       <div className={ styles.container }>
